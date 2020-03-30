c6184d7135329a895db5c6b45774ea40e390047a
kangzhenhao <1592025829@qq.com>
2020-03-30 22:37:49 +0800

    删除组件重新编码

---
+++ src/plugins/element.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import {
+  Button,
+  Container,
+  Header,
+  Main,
+  Footer,
+  Avatar,
+  Drawer,
+  Menu,
+  MenuItem,
+  Carousel,
+  CarouselItem
+} from 'element-ui'
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Button)
+Vue.use(Avatar)
+Vue.use(Drawer)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home_container">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="less" scoped>
+</style>
